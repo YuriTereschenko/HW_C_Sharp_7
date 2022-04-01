8053513ec18aee726ee1c680c11549e3c4892b54
Юрий Терещенко <yuri.tereschenko@icloud.com>
2022-04-01 22:02:16 +0500

    git init

---
+++ t47/Program.cs
@@ -0,0 +1,28 @@
+﻿/* Задача 47: Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+m = 3, n = 4.
+0,5 7 -2 -0,2
+1 -3,3 8 -9,9
+8 7,8 -7,1 9 */
+
+System.Console.WriteLine("Введите высоту массива");
+int height = Convert.ToInt32(Console.ReadLine());
+System.Console.WriteLine("Введите ширину массива");
+int weight = Convert.ToInt32(Console.ReadLine());
+double[,] array = new double[height, weight];
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < weight; j++)
+    {
+        array[i, j] = new Random().NextDouble();
+    }
+}
+
+for (int i = 0; i < height; i++)
+{
+    for (int j = 0; j < weight; j++)
+    {
+        System.Console.Write($"{array[i, j]} ");
+    }
+    System.Console.WriteLine();
+}
---
+++ t50/Program.cs
@@ -0,0 +1,49 @@
+﻿/* Задача 50: Напишите программу, которая на вход принимает позиции элемента в двумерном массиве, и возвращает значение этого элемента или же указание, что такого элемента нет.
+Например, задан массив:
+1 4 7 2
+5 9 2 3
+8 4 2 4
+
+17 -> такого числа в массиве нет */
+
+int[,] array = new int[3, 4];
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        array[i, j] = new Random().Next(1, 10);
+    }
+}
+
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        System.Console.Write($"{array[i, j]} ");
+    }
+    System.Console.WriteLine();
+}
+
+System.Console.WriteLine("Какой эллимент ищем?");
+int lookingNumber = Convert.ToInt32(Console.ReadLine());
+int count = 0;
+bool isHere = false;
+for (int i = 0; i < array.GetLength(0); i++)
+{
+    for (int j = 0; j < array.GetLength(1); j++)
+    {
+        count++;
+        if (count == lookingNumber)
+        {
+            System.Console.WriteLine(array[i, j]);
+            isHere = true;
+            break;
+        }
+    }
+}
+
+if (isHere == false)
+{
+    System.Console.WriteLine("Такого числа в массиве нет");
+}
